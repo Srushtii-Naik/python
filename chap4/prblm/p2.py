2f246d0888ee77dd1ae7c02c241906fa905505f0
Srushtii-Naik <srushtinaikk@gmail.com>
2026-07-27 21:21:26 +0530

    Added chap9

--- chap4/prblm/p2.py
+++ chap4/prblm/p2.py
@@ -1,27 +1,34 @@
-#marks in sub nd sort
+# #marks in sub nd sort
 
-marks = []
-f1 = int(input("Enter marks in sub 1: "))
-marks.append(f1)
-f2 = int(input("Enter marks in sub 2: "))
-marks.append(f2)
-f3 = int(input("Enter marks in sub 3: "))
-marks.append(f3)
-f4 = int(input("Enter marks in sub 4: "))
-marks.append(f4)
-f5 = int(input("Enter marks in sub 5: "))
-marks.append(f5)
-f6 = int(input("Enter marks in sub 6: "))
-marks.append(f6)
-f7 = int(input("Enter marks in sub 7: "))
-marks.append(f7)
+# marks = []
+# f1 = int(input("Enter marks in sub 1: "))
+# marks.append(f1)
+# f2 = int(input("Enter marks in sub 2: "))
+# marks.append(f2)
+# f3 = int(input("Enter marks in sub 3: "))
+# marks.append(f3)
+# f4 = int(input("Enter marks in sub 4: "))
+# marks.append(f4)
+# f5 = int(input("Enter marks in sub 5: "))
+# marks.append(f5)
+# f6 = int(input("Enter marks in sub 6: "))
+# marks.append(f6)
+# f7 = int(input("Enter marks in sub 7: "))
+# marks.append(f7)
 
-print(marks)
+# print(marks)
 
-#sorting opt 1:
-marks.sort()
-print("marks in sorted: ",marks)
+# #sorting opt 1:
+# marks.sort()
+# print("marks in sorted: ",marks)
 
-#sorting opt 2:
-print("sorted: ",sorted(marks))
+# #sorting opt 2:
+# print("sorted: ",sorted(marks))
 
+lst = []
+n = int(input("Enter the num of sub: "))
+for i in range(n):
+    marks = int(input(f"enter marks in sub {i+1}: "))
+    lst.append(marks)
+lst.sort()
+print(f"Sorted list : {lst}")   
