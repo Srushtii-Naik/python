16901501db5f82524c4fe2b85065c50d0dbc8b17
Srushtii-Naik <srushtinaikk@gmail.com>
2026-07-28 23:20:18 +0530

    Added file

--- chap3/Accessing_Char.py
+++ chap3/Accessing_Char.py
@@ -6,3 +6,14 @@ print(a[0])
 print(a[4])
 print(a[8])
 print(a[-1]) #last char
+
+
+#Unpacking Characters
+language = "python"
+a,b,c,d,e,f = language
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
