a271770a9b4f0e8ad146a3aec3102d97270b260d
Srushtii-Naik <srushtinaikk@gmail.com>
2026-07-29 12:35:53 +0530

    Added file

---
+++ chap4/prblm/level1.py
@@ -0,0 +1,49 @@
+# #Declare an empty list
+# lst = []
+
+# #Declare a list with more than 5 items
+# lst = ['A',23,'Srushti',65.89,'CSE']
+
+# #Find the length of your list
+# print(len(lst))
+
+# #Get the first item, the middle item and the last item of the list
+# lst = ['A',23,'Srushti',65.89,'CSE']
+# print(lst[0])
+# mid = len(lst) // 2
+# print(lst[mid])
+# print(lst[-1])
+
+# #Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+# mixed_data_types = ['Srushti',20,'5.8','None','Banglore']
+# print(mixed_data_types)
+
+# #Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+# it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle' ,'Amazon']
+# print(it_companies)
+
+#Print the number of companies in the list
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle' ,'Amazon']
+print(len(it_companies))
+
+#Print the first, middle and last company
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle' ,'Amazon']
+print(it_companies[0])
+mid = len(it_companies)//2
+print(it_companies[mid])
+print(it_companies[-1])
+
+#it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle' ,'Amazon']
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle' ,'Amazon']
+it_companies.append('MindMesh')
+print(it_companies)
+
+#Add an IT company to it_companies
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle' ,'Amazon']
+it_companies.append('Infosys')
+print(it_companies)
+
+#Insert an IT company in the middle of the companies list
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle' ,'Amazon']
+mid = len(it_companies)//2
+it_companies.appen
