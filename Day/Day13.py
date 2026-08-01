25591439304abbabc24fc8a7e5531298d13a798e
Srushti-Naik <256094630+Srushtii-Naik@users.noreply.github.com>
2026-08-01 23:56:06 +0530

    updated  Day13 pratice  file

--- Day/Day13.py
+++ Day/Day13.py
@@ -1,18 +1,18 @@
-# #Filter only negative and zero in the list using list comprehension
-# numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
-# negatives_and_zero = [ n for n in numbers if n <= 0]
-# print(negatives_and_zero)
+#Filter only negative and zero in the list using list comprehension
+numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
+negatives_and_zero = [ n for n in numbers if n <= 0]
+print(negatives_and_zero)
 
-# #Flatten the following list of lists of lists to a one dimensional list :
-# list_of_lists =[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-# flatened_list = [num for sublist in list_of_lists for num in sublist]
-# print(flatened_list)
+#Flatten the following list of lists of lists to a one dimensional list :
+list_of_lists =[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flatened_list = [num for sublist in list_of_lists for num in sublist]
+print(flatened_list)
 
 
-# #Using list comprehension create the following list of tuples:
-# 'List of tuples (powers)'
-# tuple_list = [(i, 1, i, i**2, i**3, i**4, i**5) for i in range(11)]
-# print(tuple_list)
+#Using list comprehension create the following list of tuples:
+'List of tuples (powers)'
+tuple_list = [(i, 1, i, i**2, i**3, i**4, i**5) for i in range(11)]
+print(tuple_list)
 
 
 #Flatten the following list to a new list:
